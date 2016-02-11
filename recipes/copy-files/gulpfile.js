a5107041b69db1c93c9a35c9f56c220f94e2560f
Yoshihide Jimbo <yjimbo@gmail.com>
2016-02-11 15:25:30 +0900

    Copy files to multiple directories

--- recipes/copy-files/gulpfile.js
+++ recipes/copy-files/gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp');
 
 function copy() {
   return gulp.src('src/**/*')
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('backup'));
 }
 
-copy.description = 'Copy files to another directory.'
+copy.description = 'Copy files to other directories.'
 
 gulp.task('default', copy);
